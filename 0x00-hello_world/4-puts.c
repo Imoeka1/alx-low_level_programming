85dac950aa18b29418119034fc160beae73c0593
Imoeka1 <Imoekanem8@gmail.com>
2022-09-09 11:12:50 -0700

    task for

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-void 4-puts.c
-
 #include <stdio.h>
-
+/**
+*main- Serves as the entry point for the program
+*
+*Return: By default returns zero if no errors otherwise returns non-zero value.
+*/
+int main(void)
+{
+ puts("\"Programming is like building a miltilingual puzzle");
+ return (0);
+}
