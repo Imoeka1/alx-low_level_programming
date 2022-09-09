949381eb4dc57d53841458d4758781bfb2b71f9d
Imoeka1 <Imoekanem8@gmail.com>
2022-09-09 06:19:37 -0700

    task five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
 * main - print the string in the put function
-*
+
 * Description: using the main function
-* this program prints "Programming is like building a multiligual puzzle
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
