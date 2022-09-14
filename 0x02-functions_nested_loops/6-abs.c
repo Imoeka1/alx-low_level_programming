00ab1c027898797bfa8ff327215c71ea18337ced
Imoeka1 <Imoekanem8@gmail.com>
2022-09-14 14:37:54 -0700

    task six

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @ab: input Letrer
+ * Return: Always Success
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-function that prints
+functions that computes integer
