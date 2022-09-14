b81944c2632486d491207fa6a62ba9b325cbb161
Imoeka1 <Imoekanem8@gmail.com>
2022-09-14 10:59:05 -0700

    task putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,16 @@
-scripr thar returns
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success
+ * */
+int main(void)
+{
+
+	printf("_putchar\n");
+
+	return (0);
+}
+
